e5a45e7d847136ea717e130ee3db5d66241b7383
juniormince <groundcontroltomajorbree@gmail.com>
2018-05-10 11:46:40 -0500

    add angular connection

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,12 @@
 console.log('js czeck');
+
+var app = angular.module('FilmCollectionApp', ['ngRoute']);
+
+app.controller('TestController', ['$http', function ($http)  {
+console.log('angular czeck');
+
+var self = this;
+
+self.message = 'moviessss';
+
+}]);
